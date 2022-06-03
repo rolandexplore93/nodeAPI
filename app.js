d71da4ed99669370371d98a5612de669cc41bfe9
rolandexplore93 <roland2rule@gmail.com>
2022-06-03 01:18:39 +0100

    Implement GET, POST & DELETE request using POSTMAN

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { ObjectId } = require('mongodb')
 // initialize express app & middleware
 const app = express()
 app.use(express.urlencoded({ extended: true}))
+app.use(express.json());
 // db connection
 let db;
 connectToDb((err) => {
@@ -30,6 +31,7 @@ app.get('/books', (req, res) => {
     })
     
 });
+
 app.get('/books/:id', (req, res) => {
     const id = req.params.id
     if(ObjectId.isValid(req.params.id)){
@@ -43,5 +45,35 @@ app.get('/books/:id', (req, res) => {
             })
     } else {
         res.status(500).json({error: "Document not found"})
+    }
+})
+
+app.post('/books', (req, res) => {
+    const book = req.body;
+
+    db.collection('books')
+        .insertOne(book)
+        .then((result) => {
+            res.status(200).json(result)
+        })
+        .catch(() => {
+            res.status(500).json({ error: "Couldn't create a new document"});
+        })
+})
+
+app.delete('/books/:id', (req, res) => {
+    const id = req.params.id
+    
+    if(ObjectId.isValid(req.params.id)){
+        db.collection('books')
+            .deleteOne({_id: ObjectId(id)})
+            .then((result) => {
+                res.status(200).json(result)
+            })
+            .catch((error) => {
+                res.status(500).json({ error: "Couldn't delete the document from database"});
+            })
+    } else {
+        res.status(500).json({error: "Document is not valid"})
     }
 })
